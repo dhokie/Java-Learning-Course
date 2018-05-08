2898d86b93d797c3fbd3b07b09e2e6d3a3ca6c9d
TSI <josereyamorganda@gmail.com>
2018-05-08 14:13:42 +0800

    This is my second commit, just a simple comment.

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,6 +2,7 @@
  * Created by TSI on 5/8/2018.
  */
 public class helloWorld {
+    // This is a test comment. I'm practicing with Git!
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
