7764203b200dd81e786f3a0d82fc747883ed56da
TSI <josereyamorganda@gmail.com>
2018-05-11 10:57:04 +0800

    Inheritance

---
+++ src/Polymorphism/Inheritance.java
@@ -0,0 +1,32 @@
+package Polymorphism;
+
+/**
+ * Created by TSI on 5/11/2018.
+ */
+public class Inheritance {
+
+    public static void main(String args[ ]) {
+        Animal a = new Dog();
+        Animal b = new Cat();
+
+        a.makeSound();
+        b.makeSound();
+    }
+}
+
+class Animal {
+    public void makeSound() {
+        System.out.println("Grr...");
+    }
+}
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
