016f3545088707e41e6d209de40a54808fd7ea93
TSI <josereyamorganda@gmail.com>
2018-05-11 10:35:32 +0800

    ArrayList

---
+++ src/Arrays/Test_ArrayList.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+import java.util.ArrayList;
+class Test_ArrayList {
+    public static void main(String[] args) {
+        //Creating a generic ArrayList
+        ArrayList arrayList = new ArrayList();
+        //Size of arrayList
+        System.out.println("Size of ArrayList at creation: " + arrayList.size());
+        //Lets add some elements to it
+        arrayList.add("J");
+        arrayList.add("O");
+        arrayList.add("S");
+        arrayList.add("E");
+
+        //Recheck the size after adding elements
+        System.out.println("Size of ArrayList after adding elements: " + arrayList.size());
+
+        //Display all contents of ArrayList
+        System.out.println("List of all elements: " + arrayList);
+
+        //Remove some elements from the list
+        arrayList.remove("J");
+        System.out.println("See contents after removing one element: " + arrayList);
+
+        //Remove element by index
+        arrayList.remove(2);
+        System.out.println("See contents after removing element by index: " + arrayList);
+
+        //Check size after removing elements
+        System.out.println("Size of arrayList after removing elements: " + arrayList.size());
+        System.out.println("List of all elements after removing elements: " + arrayList);
+
+        //Check if the list contains "S"
+        System.out.println(arrayList.contains("S"));
+
+        if(arrayList.contains("O")){
+            System.out.println("Found a letter");
+        } else {
+            System.out.println("No match found");
+        }
+
+    }
+}
