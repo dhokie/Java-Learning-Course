b19fd8a20d8512baac589d9a495524f9347f7dde
TSI <josereyamorganda@gmail.com>
2018-05-17 12:13:16 +0800

    Abstract Class Method sample code

---
+++ src/AbstractClassMethod/AbstractClassMethod.java
@@ -0,0 +1,18 @@
+package AbstractClassMethod;
+
+/**
+ * Created by TSI on 5/17/2018.
+ */
+public class AbstractClassMethod {
+
+    public static void main(String args[]){
+        AbstractService test = new PublicClassDhokie();
+
+        test.joseRey();
+        test.jamesCarlo();
+        test.tonyZerna();
+        test.sirAjay();
+
+    }
+
+}
