507ab8c39e00db5e735fcc24a91d10ebba7b6f3a
TSI <josereyamorganda@gmail.com>
2018-05-17 14:12:30 +0800

    Interface sample code

--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
     public static void main(String arg[]){
         InterfaceService anInterface = new InterfaceService();
-        anInterface.firstMethod();
+        anInterface.firstMethod(); // calling the fisrt method in the interfaceService implementing interfaceServiceImpl
     }
 }
